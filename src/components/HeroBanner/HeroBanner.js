314239c6f2b723c2a5f2ae3b92c10517412faf97
joshganote <j.ganote0825@gmail.com>
2022-06-09 02:38:53 -0500

    refactored navbar to be fixed and as well as app width and hero image

---
+++ src/components/HeroBanner/HeroBanner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import test from "../../assets/Images/image1.png";
+import "./HeroBanner.css";
+
+export const HeroBanner = () => {
+  return (
+    <div>
+      <div className="hero-banner">
+        <h1>A.M. Paradox</h1>
+      </div>
+    </div>
+  );
+};
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,21 +7,23 @@ import "./Navbar.css";
 export const Navbar = () => {
   const content = useContext(ContentContext);
   return (
-    <div className="nav-container">
-      <img src={snyderLogoBlue} alt="snyder recordings logo" className="logo" />
-      <div className="link-container">
-        {content.navbar.route.map((link, index) => (
-          <div key={index} className="nav-links">
-            <img src={link.icon} alt="nav icon" />
-            <a className="links" href={link.path}>
-              {link.name}
-            </a>
-          </div>
-        ))}
+    <div style={{ position: "fixed", zIndex: 10, width: "100%" }}>
+      <div className="nav-container">
+        <img src={snyderLogoBlue} alt="snyder recordings logo" className="logo" />
+        <div className="link-container">
+          {content.navbar.route.map((link, index) => (
+            <div key={index} className="nav-links">
+              <img src={link.icon} alt="nav icon" />
+              <a className="links" href={link.path}>
+                {link.name}
+              </a>
+            </div>
+          ))}
+        </div>
+        <a className="sign-up links" href={content.navbar.signUp.path}>
+          {content.navbar.signUp.name}
+        </a>
       </div>
-      <a className="sign-up links" href={content.navbar.signUp.path}>
-        {content.navbar.signUp.name}
-      </a>
     </div>
   );
 };
