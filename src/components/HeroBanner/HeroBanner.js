9413b1b2f63ddbaa0bafd50b32cd1ff259fef16f
joshganote <j.ganote0825@gmail.com>
2022-06-09 15:17:27 -0500

    hero responsive finished

--- src/components/HeroBanner/HeroBanner.js
+++ src/components/HeroBanner/HeroBanner.js
@@ -1,24 +1,28 @@
 import React from "react";
+import play from "../../assets/Icons/SVGs/playbutton.svg";
 import "./HeroBanner.css";
 
 export const HeroBanner = () => {
+  const handleClick = () => {
+    /**
+     * Perform task
+     */
+  };
+
   return (
     <div className="hero-banner">
       <div className="hero-wrapper">
-        <div className="hero-header">
-          <p className="header-txt">A.M. Paradox</p>
-        </div>
+        <p className="a-m-paradox">A.M. Paradox</p>
 
-        <div className="hero-description">
-          <p className="description-txt">
-            Christopher Brent Wood, better known by his stage name Brent Faiyaz, is an American
-            singer and record producer. He released his debut project, an extended play entitled
-            A.M. Paradox, in 2016.
-          </p>
-        </div>
+        <p className="christopher-brent-wo">
+          Christopher Brent Wood, better known by his stage name Brent Faiyaz, is an American singer
+          and record producer. He released his debut project, an extended play entitled A.M.
+          Paradox, in 2016.
+        </p>
 
-        <div className="hero-play">
-          <p className="play-txt">Watch Video</p>
+        <div className="hero-play" onClick={handleClick}>
+          <img src={play} alt="play button" className="group-4-copy-4" />
+          <p className="watch-video">Watch Video</p>
         </div>
       </div>
     </div>
