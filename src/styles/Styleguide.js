55f8bddcf66a8eed5e98c25775836d3f28672291
joshganote <j.ganote0825@gmail.com>
2022-06-09 00:37:58 -0500

    navbar complete

---
+++ src/styles/Styleguide.js
@@ -0,0 +1,15 @@
+export const Styleguide = {
+  colors: {
+    primaryBlue: "#001D5D",
+    lightGrey: "#F6F6F6",
+    mediumGrey: "#B8BFCF",
+    darkGrey: "#525252",
+    primaryGreen: "#68C242",
+    secondaryGreen: "#D9F0D0",
+    black: "#000000",
+    white: "#FFFFFF",
+  },
+  typography: {
+    fontFamily: "Karla",
+  },
+};
