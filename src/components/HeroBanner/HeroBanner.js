9c8ea30cd83062a2b4467e39c562dfd2e39c67f4
joshganote <j.ganote0825@gmail.com>
2022-06-09 13:29:38 -0500

    fixing margins

--- src/components/HeroBanner/HeroBanner.js
+++ src/components/HeroBanner/HeroBanner.js
@@ -1,11 +1,26 @@
 import React from "react";
-import test from "../../assets/Images/image1.png";
 import "./HeroBanner.css";
 
 export const HeroBanner = () => {
   return (
     <div className="hero-banner">
-      <h1>A.M. Paradox</h1>
+      <div className="hero-wrapper">
+        <div className="hero-header">
+          <p className="header-txt">A.M. Paradox</p>
+        </div>
+
+        <div className="hero-description">
+          <p className="description-txt">
+            Christopher Brent Wood, better known by his stage name Brent Faiyaz, is an American
+            singer and record producer. He released his debut project, an extended play entitled
+            A.M. Paradox, in 2016.
+          </p>
+        </div>
+
+        <div className="hero-play">
+          <p className="play-txt">Watch Video</p>
+        </div>
+      </div>
     </div>
   );
 };
